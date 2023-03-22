2eefc0b6b7de27b80eeed6fb1007118f9e6fb8f4
sammygee0245 <samuelbediako020@gmail.com>
2023-03-22 22:03:54 +0000

    dd

---
+++ index.js
@@ -0,0 +1,53 @@
+// navigation dropdown effect
+let bars = document.querySelector("#bars");
+let ul2 = document.querySelector("#ul2");
+let book = document.querySelector("#book");
+
+let showNav = false;
+
+function displayNav() {
+  if (!showNav) {
+    ul2.style.top = 141 + "px";
+    book.style.display = "flex";
+    showNav = !showNav;
+  } else {
+    ul2.style.top = -1000 + "px";
+    book.style.display = "none";
+    showNav = !showNav;
+  }
+}
+
+bars.addEventListener("click", displayNav);
+
+// gsddsgds?
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+});
